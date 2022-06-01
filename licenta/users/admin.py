d5ba1c13cd0859145dec9592c7e2d10e1264bbcf
MaraHermeneanu <57602931+MaraHermeneanu@users.noreply.github.com>
2022-06-01 17:31:15 +0300

    Delete licenta directory

--- licenta/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile,CameraParameters,ChessboardImage,StereoCameraParameters
-
-admin.site.register(Profile)
-admin.site.register(CameraParameters)
-admin.site.register(StereoCameraParameters)
-admin.site.register(ChessboardImage)
